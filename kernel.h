c15e7809853115d2b865d104094bc64a6a1c84e4
euklid <r@twopi.eu>
2015-07-25 20:01:17 +0200

    added translation operator declarations

--- kernel.h
+++ kernel.h
@@ -3,12 +3,18 @@
 
 #include "element.h"
 #include "complex_t.h"
+#include "cell.h"
 
 class Kernel
 {
 public:
     Kernel(){}
     virtual double direct(Element const & el1, Element const & el2) = 0;
+    virtual complex_t direct_cmp(Element const & el1, Element const & el2) = 0;
+    virtual void M2M(Cell const & first, Cell & second) = 0;
+    virtual void M2L(Cell const & first, Cell & second) = 0;
+    virtual void L2L(Cell const & first, Cell & second) = 0;
+
 };
 
 #endif
