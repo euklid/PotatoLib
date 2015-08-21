c3fd9c23ffc9b08c5c0e76ff5cbe2708d1bb5838
euklid <r@twopi.eu>
2015-08-22 01:53:52 +0200

    better data creation

--- TESTING/create_data.py
+++ TESTING/create_data.py
@@ -1,43 +1,56 @@
 #!/usr/bin/python
 import random
 
-num_data = 1000
-max_leaf = 10
-exp_terms = 15
+num_data = 50
+max_leaf = 12
+exp_terms = 6
 loc_terms = exp_terms
+
+data_box_size = 60000.0
 
 
 f = open('fmm_data.conf','w')
 f.write("0\n")
 
 for i in range(num_data):
-    x = random.gauss(3,1)
-    if x < 0:
-        x = 0
-    if x > 6:
-        x = 0
-    y = random.gauss(3,1)
-    if y <0:
-        y = 0
-    if y > 6:
-        y = 0
-    init_val = random.random()
+    invalid = True
+    x = 0
+    y = 0
+    while(invalid) :
+        x = random.gauss(data_box_size/2,data_box_size/4)
+        if x < 0:
+            continue
+        if x > data_box_size:
+            continue
+        y = random.gauss(data_box_size/2,data_box_size/4)
+        if y <0:
+            continue
+        if y > data_box_size:
+            continue
+        invalid = False
+    init_val = 0.01 #random.random()/num_data
     s = 1
     t = 0
     line = str(x) + " " + str(y) + " " + str(s) + " " + str(t) + " " + str(init_val)+"\n"
     f.write(line)
+
     
 for i in range(num_data):
-    x = random.gauss(3,1)
-    if x < 0:
-        x = 0
-    if x > 6:
-        x = 0
-    y = random.gauss(3,1)
-    if y <0:
-        y = 0
-    if y > 6:
-        y = 0
+    invalid = True
+    x = 0
+    y = 0
+    while(invalid) :
+        x = random.gauss(data_box_size/2,data_box_size/4)
+        if x < 0:
+            continue
+        if x > data_box_size:
+            continue
+        y = random.gauss(data_box_size/2,data_box_size/4)
+        if y <0:
+            continue
+        if y > data_box_size:
+            continue
+        invalid = False
     s = 0
     t = 1
     line = str(x) + " " + str(y) + " " + str(s) + " " + str(t) + " 0" + "\n"
